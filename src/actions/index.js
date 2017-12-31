cf6ee940b94dd3c75f85f9eb0b45f5e5dd932fd6
Arnold Cano <arnoldcano@yahoo.com>
2017-12-31 16:15:31 -0600

    remove key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '2643d3f763358f5fb1f2841f607648c1';
+const API_KEY = 'changme';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
